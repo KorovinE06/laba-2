027060b6c55d401ee1bed4e0681084c98835d125
KorovinE06 <egorkorovinvostok@gmail.com>
2024-12-20 14:48:10 +0500

    Add files via upload

---
+++ Лабараторная 2/Лаба 2 Рекурсия/Лаба 2 Рекурсия/Лаба 2 Рекурсия.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<clocale>
+#include<math.h>
+using namespace std;
+int n;
+double x;
+
+double rec(int n, double x)
+{
+	if (n == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return cos((2 * n - 1) * x) / (pow(2 * n - 1, 2)) + rec(n - 1, x);
+	}
+}
+int main()
+{
+	cout << "Enter n: ";
+	cin >> n;
+	cout << "Enter x: ";
+	cin >> x;
+	double su = rec(n, x);
+	cout << su;
+	return 0;
+}
